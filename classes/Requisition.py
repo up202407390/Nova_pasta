7524809dc974989a972f13d7f9bfa64341b4eec6
Carolina Lima <up202407390@up.pt>
2025-04-05 15:31:49 +0100

    Primeiro Commit

---
+++ classes/Requisition.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  3 19:40:39 2025
+
+@author: Utilizador
+"""
+
+
+from classes.gclass import Gclass
+
+class Requisition(Gclass):
+    
+    obj = dict()
+    lst = list()
+    pos = 0
+    sortkey = ''
+    # Attribute names list, identifier attribute must be the first one and callled 'id'
+    att = ['_member_id', '_borrow_date', '_book_id', '_return_date']
+    # Class header title
+    header = 'Requisitions'
+    # field description for use in, for example, input form
+    des = ['Member id', 'Borrow date', 'Book id', 'Return date']
+    
+    
+    def __init__(self, member_id, borrow_date, book_id, return_date):
+        super().__init__()
+        
+        member_id = Requisition.get_id(member_id)
+        self._member_id = member_id
+        self._borrow_date = borrow_date
+        self._book_id = book_id
+        self._return_date = return_date 
+        
+        Requisition.obj[member_id] = self
+        # Add the id to the list of object ids
+        Requisition.lst.append(member_id)
+        
+    @property
+    def member_id(self):
+        return self._member_id
+    @member_id.setter
+    def member_id(self, member_id):
+        self._member_id = member_id
+        
+    @property
+    def borrow_date(self):
+        return self._borrow_date
+    @borrow_date.setter
+    def borrow_date(self, borrow_date):
+        self._borrow_date = borrow_date
+        
+    @property
+    def book_id(self):
+        return self._book_id
+    @book_id.setter
+    def book_id(self, book_id):
+        self._book_id = book_id
+        
+    @property
+    def return_date(self):
+        return self._return_date
+    @return_date.setter
+    def return_date(self, return_date):
+        self._return_date = return_date
